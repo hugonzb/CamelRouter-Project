69a26907d929d7246c98b73346ceb15bf020dec8
Hugo <hugonzb@gmail.com>
2020-05-05 14:50:56 +1200

    Created base layout of table in HTML for customer display Fixed up URIs in javascript

--- accounts-ajax/public/js/customer.js
+++ accounts-ajax/public/js/customer.js
@@ -1,18 +1,18 @@
 "use strict";
 
-let module = angular.module('Customer', ['ngResource']);
+let module = angular.module('Customers', ['ngResource']);
 
 module.factory('getCustomers', function ($resource) {
-    return $resource('http://localhost:7081/api/customer');
+    return $resource('http://localhost:7081/api/accounts');
 });
 
 module.factory('createCustomer', function ($resource) {
-    return $resource('http://localhost:7081/api/customer');
+    return $resource('http://localhost:7081/api/accounts');
 });
 
 module.controller('CustomerController', function (getCustomers, createCustomer) {
     this.customers - getCustomers.query();
-    this.createNewCustomer = function (customer) {
+    this.createAccount = function (customer) {
         createCustomer.save(null, customer, function() {
             this.customers = getCustomers.query();
         });
