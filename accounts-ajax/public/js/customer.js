8e34c225f2c7a9a927d553855259746f8f566578
Hugo <hugonzb@gmail.com>
2020-05-05 14:38:00 +1200

    Added customer controllers for get customers and create customer

--- accounts-ajax/public/js/customer.js
+++ accounts-ajax/public/js/customer.js
@@ -1,1 +1,20 @@
 "use strict";
+
+let module = angular.module('Customer', ['ngResource']);
+
+module.factory('getCustomers', function ($resource) {
+    return $resource('http://localhost:7081/api/customer');
+});
+
+module.factory('createCustomer', function ($resource) {
+    return $resource('http://localhost:7081/api/customer');
+});
+
+module.controller('CustomerController', function (getCustomers, createCustomer) {
+    this.customers - getCustomers.query();
+    this.createNewCustomer = function (customer) {
+        createCustomer.save(null, customer, function() {
+            this.customers = getCustomers.query();
+        });
+    };
+});
