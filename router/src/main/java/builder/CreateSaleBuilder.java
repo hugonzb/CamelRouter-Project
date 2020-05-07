478e5a49521da5229fd66f3efde38d66b2e6879b
Hugo <hugonzb@gmail.com>
2020-05-07 18:54:23 +1200

    Extracted and stored sale attributes in exchange properties

--- router/src/main/java/builder/CreateSaleBuilder.java
+++ router/src/main/java/builder/CreateSaleBuilder.java
@@ -1,20 +1,30 @@
 package builder;
 
+import domain.Sale;
 import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 
 public class CreateSaleBuilder extends RouteBuilder{
     @Override
     public void configure() throws Exception {
         from("imaps://outlook.office365.com?username=<username>@student.otago.ac.nz"
-        + "&password=" + getPassword("Enter your E-Mail password")
-        + "&searchTerm.subject=Vend:SaleUpdate"
-        + "&debugMode=false"  // set to true if you want to see the authentication details
-        + "&folderName=INBOX")  // change to whatever folder your Vend messages end up in
-        .convertBodyTo(String.class)
-        .log("${body}")
-        .to("jms:queue:vend-new-sale");
+            + "&password=" + getPassword("Enter your E-Mail password")
+            + "&searchTerm.subject=Vend:SaleUpdate"
+            + "&debugMode=false"  // set to true if you want to see the authentication details
+            + "&folderName=INBOX")  // change to whatever folder your Vend messages end up in
+            .convertBodyTo(String.class)
+            .log("${body}")
+            .to("jms:queue:vend-new-sale");
+        from("jms:queue:vend-new-sale")
+            .unmarshal().json(JsonLibrary.Gson, Sale.class)
+            .setProperty("Customer_Id").simple("${body.customer.id}")
+            .setProperty("Customer_Group").simple("${body.customer.group}")
+            .setProperty("Customer_Email").simple("${body.customer.email}")
+            .setProperty("Customer_FirstName").simple("${body.customer.firstname}")
+            .setProperty("Customer_LastName").simple("${body.customer.lastname}")
+            .to("jms:queue:sale-data");
         
 
     }
